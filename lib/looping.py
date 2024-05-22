299d310c70c8b08cba7d9a9624075830554e6c32
Carolyne munyiva <munyivacarolyne571@gmail.com>
2024-05-22 23:52:48 +0300

    complete py loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")    
     # code goes here!
     pass
 
 def square_integers(int_list):
+    return[num **2 for num in int_list]
     # code goes here!
     pass
 
 def fizzbuzz():
+    for num in range(1,101):
+        print(fizzbuzz(num))
     # code goes here!
     pass
